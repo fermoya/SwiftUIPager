1604644a8da0376f973b42b2e30a9b0c182cc53f
fred-bowker <fred.bowker@locovo.co>
2022-03-26 16:30:16 +0000

    Feature allow for scrolling forward only
    updating .gitignore file for AppCode IDE
    
    * Update git ignore to ignore .idea folder
    * Added missing file in project ForwardOnlyExampleView.swift
    
    Minor refactor, forwardOnly renamed to dragForwardOnly
    
    Added parameter comment to dragForwardOnly
    
    dragForwardOnly moved inside #if !os(tvOS)

---
+++ Example/SwiftUIPagerExample/Examples/DragForwardOnlyExampleView.swift
@@ -0,0 +1,44 @@
+//  DragForwardOnlyExampleView.swift
+//  SwiftUIPagerExample
+//
+//  Created by  Fred Bowker on 26/03/2022..
+import SwiftUI
+
+struct DragForwardOnlyExampleView: View {
+    
+    @StateObject var page: Page = .first()
+    
+    var colors: [Color] = [
+        .red, .blue, .black, .gray, .purple, .green, .orange, .pink, .yellow, .white
+    ]
+    
+    var body: some View {
+        NavigationView {
+            GeometryReader { proxy in
+                VStack(spacing: 10) {
+                    Pager(page: self.page,
+                          data: self.colors,
+                          id: \.self) {
+                        self.pageView($0)
+                    }
+                          .contentLoadingPolicy(.eager)
+                          .itemSpacing(10)
+                          .padding(20)
+                          .dragForwardOnly(true)
+                          .frame(width: min(proxy.size.height, proxy.size.width),
+                                 height: min(proxy.size.height, proxy.size.width))
+                          .background(Color.gray.opacity(0.3))
+                          .navigationBarTitle("Move Forward Only", displayMode: .inline)
+                }
+            }
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+    
+    func pageView(_ color: Color) -> some View {
+        Rectangle()
+            .fill(color)
+            .cornerRadius(5)
+            .shadow(radius: 5)
+    }
+}
