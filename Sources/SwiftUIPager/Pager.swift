abc2471f9b7b9177a163586a0e533e2810c3955e
Nathan Mak <nathan_mak@icloud.com>
2023-09-23 19:09:50 +0800

    perf: remove clipped modifier

--- Sources/SwiftUIPager/Pager.swift
+++ Sources/SwiftUIPager/Pager.swift
@@ -190,7 +190,7 @@ public struct Pager<Element, ID, PageView>: View  where PageView: View, Element:
         GeometryReader { proxy in
             self.content(for: proxy.size)
         }
-        .clipped()
+       // .clipped()
     }
 
     func content(for size: CGSize) -> PagerContent {
